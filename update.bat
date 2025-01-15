37f4a1381afb8c828a12462c4dd6368b1e2b1b8a
Michaël AUGUSTE <github.ej675@passmail.net>
2025-01-15 01:28:56 +0100

    test3

--- update.bat
+++ update.bat
@@ -4,6 +4,8 @@ chcp 65001 > nul
 :: Chemins absolus de l'ancien et du nouveau fichier exécutables
 set old_file_path=%1
 set new_file_path=%2
+echo old : %old_file_path%
+echo new : %new_file_path%
 timeout 10
 :: Chemins absolus de l'ancien et du nouveau dossier parent
 for %%i in ("%old_file_path%") do set "old_filedir=%%~dpi"
