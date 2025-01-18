eed5345c0211a951edfbd4b09946c6105910693f
Michaël AUGUSTE <github.ej675@passmail.net>
2025-01-18 02:04:17 +0100

    test

--- update.bat
+++ update.bat
@@ -1,12 +1,12 @@
-@echo off
-::chcp 65001 > nul
-::
-:::: Chemins absolus de l'ancien et du nouveau fichier exécutables
-::set old_file_path=%1
-::set new_file_path=%2
-::echo old : %old_file_path%
-::echo new : %new_file_path%
-::timeout 10
+::@echo off
+chcp 65001 > nul
+
+:: Chemins absolus de l'ancien et du nouveau fichier exécutables
+set old_file_path=%1
+set new_file_path=%2
+echo old : %old_file_path%
+echo new : %new_file_path%
+timeout 10
 :::: Chemins absolus de l'ancien et du nouveau dossier parent
 ::for %%i in ("%old_file_path%") do set "old_filedir=%%~dpi"
 ::for %%i in ("%new_file_path%") do set "new_filedir=%%~dpi"
@@ -48,4 +48,4 @@
 ::) else (
 ::    echo Le dossier n'existe pas : %new_filedir%
 ::)
-pause
+::pause
